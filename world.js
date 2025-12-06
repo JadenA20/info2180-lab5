1ae790abc1260046c8d7c95226a2aff98fcd4be0
Jaden <jaden.anthony2@gmail.com>
2025-12-05 20:08:36 -0500

    Update world.js

--- world.js
+++ world.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+  const lookupButton = document.getElementById("lookup");
+  const userInput = document.getElementById("country");
+  const queryResult = document.getElementById("result");
+
+  //AJAX Request
+  function lookupListener(){
+    const userRequest = new XMLHttpRequest();
+    const userQuery = userInput
+
+    userRequest.onreadystatechange = function() {
+      if (userRequest.readyState === XMLHttpRequest.DONE) {
+        if (userRequest.status === 200) {
+          let response = userRequest.responseText;
+          result.innerHTML = response;
+        }
+        else {
+          alert('There was an issue with the request receieved');
+        }
+      }
+    }
+    userRequest.open('GET', `world.php?country=${encodeURIComponent(userQuery)}`, true);
+    userRequest.send()
+  }
+
+
+
+
+    
+}
