3dbf5f4067bdfc20aa3d343f50946e865c984860
Jaden <jaden.anthony2@gmail.com>
2025-12-05 21:02:45 -0500

    Update world.php

--- world.php
+++ world.php
@@ -1,5 +1,3 @@
-#Name: Jaden Anthony
-#ID: 620164510
 
 <?php
 $host = 'localhost';
@@ -19,11 +17,43 @@ if (isset($_GET['country']) && !empty(trim($_GET['country']))){
     $stmt->execute();
     
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($results != null){
+        #Display search from lookup
+        echo 
+        '<table class="userlookup">
+        <thead>
+        <tr>
+        <th>Country</th>
+        <th>Continent</th>
+        <th>Year of Independence</th>
+        <th>Head of State</th>
+        </tr>
+        </thead>
+        <tbody>';
+        #Prints data in HTML table
+        foreach ($results as $row) {
+                echo '<tr>
+                <td>' . htmlspecialchars($row['name']) . '</td>
+                <td>' . htmlspecialchars($row['continent']) . '</td>
+                <td>' . htmlspecialchars($row['year_of_independence']) . '</td>
+                <td>' . htmlspecialchars($row['head_of_state']) . '</td>
+                </tr>';
+            }
+            echo '</tbody></table>';
+        } 
+    else {
+        echo "Country not found";
+    }
+
+    
 }
 
+/*<ul>
+*<?php foreach ($results as $row): ?>
+* <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+*<?php endforeach; ?>
+*</ul>
+*/
+
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
